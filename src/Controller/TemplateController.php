becab36e367e3c41863781bf7001119fafa806f3
abdulrhman.al <abdulrhman.al@anton-paar.com>
2023-08-31 07:29:13 +0200

    Implementations: Added Maintenance & Incident Templates Editing

--- src/Controller/TemplateController.php
+++ src/Controller/TemplateController.php
@@ -63,27 +63,94 @@ class TemplateController extends AbstractController
         ]);
     }
 
+    #[Route("/template/{id}", name: "app_template_show")]
+    public function showTemplate(Template $template): Response
+    {
+        return $this->render("template/show.html.twig", [
+            "template" => $template,
+        ]);
+    }
 
+    /**
+     * Edit an existing template.
+     *
+     * @param Request $request The HTTP request
+     * @param Template $template The template entity to edit
+     * @param ManagerRegistry $managerRegistry The manager registry for entity management
+     * @return Response
+     */
+    #[Route("/template/{id}/edit", name: "app_template_edit")]
+    public function edit(Request $request, Template $template, ManagerRegistry $managerRegistry): Response
+    {
+        // Create a form for editing the template
+        $form = $this->createFormBuilder($template)
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'Maintenance' => 'maintenance',
+                    'Incident' => 'incident',
+                ]])
+            ->add('title', TextType::class)
+            ->add('subject', TextType::class)
+            ->add("template", TextareaType::class, [
+                "label" => "Content",
+                "attr" => ["rows" => 5],  // Add this line to set the number of rows
+            ])           
+            ->add("save", SubmitType::class, [
+                "label" => "Save",
+                "attr" => [
+                    "class" => "btn btn-primary",
+                ],
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        // Check if the form is submitted and valid
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Get the entity manager and update the template in the database
+            $entityManager = $managerRegistry->getManager();
+            $entityManager->flush();
+
+            // Redirect back to the template index page
+            return $this->redirectToRoute("app_template");
+        }
+
+        // Render the edit template form
+        return $this->render("template/edit.html.twig", [
+            "form" => $form->createView(),
+        ]);
+    }
     
+    
+    /**
+     * Get the content of a template.
+     *
+     * @param Template $template The template entity to retrieve content from
+     * @return JsonResponse The JSON response containing the template content
+     */
     #[Route("/template/{id}/get-template-content", name:"get_template_content")]
     public function getTemplateContent(Template $template): JsonResponse
     {
+        // Get the template content from the entity
         $templateContent = $template->getTemplate();
-        // $templateSubject = $template->getSubject();
-        
-        // Replace newline and carriage return characters with spaces
-        //$templateContent = str_replace(["\r\n", "\r", "\n"], ' ', $templateContent);
+               
+        // Return the template content as JSON response
         return new JsonResponse($templateContent);
     }
     
+    /**
+     * Get the subject of a template.
+     *
+     * @param Template $template The template entity to retrieve subject from
+     * @return JsonResponse The JSON response containing the template subject
+     */
     #[Route("/template/{id}/get-template-subject", name:"get_template_subject")]
     public function getTemplateSubject(Template $template): JsonResponse
     {
-        // $templateContent = $template->getTemplate();
+        // Get the template subject from the entity
         $templateSubject = $template->getSubject();
-        
-        // Replace newline and carriage return characters with spaces
-        //$templateContent = str_replace(["\r\n", "\r", "\n"], ' ', $templateContent);
+                
+        // Return the template subject as JSON response
         return new JsonResponse($templateSubject);
     }
 }
